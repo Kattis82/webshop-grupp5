d90b4956bf4dbc5245a7ed47f7299b68403fe974
Kattis82 <kattiscalmvik@outlook.com>
2026-04-08 17:05:46 +0200

    Skapat Cart och CartItem för session-baserad kundvagn

--- src/main/java/se/iths/kattis/webshopgrupp5/model/cart/CartItem.java
+++ src/main/java/se/iths/kattis/webshopgrupp5/model/cart/CartItem.java
@@ -1,4 +1,32 @@
 package se.iths.kattis.webshopgrupp5.model.cart;
 
+import lombok.Getter;
+import lombok.Setter;
+
+// en rad i kundvagnen
+@Getter
+@Setter
 public class CartItem {
+
+    private Long productId;
+    private String productName;
+    private double productPrice;
+    private int quantity;
+
+    public CartItem(Long productId, String productName, double productPrice, int quantity) {
+        this.productId = productId;
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.quantity = quantity;
+    }
+
+    // metod som ökar antal om produkten finns
+    public void increaseQuantity() {
+        this.quantity++;
+    }
+
+    // metod för att räkna ut totalpris
+    public double getTotalPrice() {
+        return productPrice * quantity;
+    }
 }
