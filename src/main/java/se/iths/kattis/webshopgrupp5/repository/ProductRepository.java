34ce97d0b1d453c85acb34e77aa1a27ff27ae35b
J0ssan-iths <Josefine.Berglund@iths.se>
2026-04-09 11:22:20 +0200

    ProductRepository fick rätt annotation

--- src/main/java/se/iths/kattis/webshopgrupp5/repository/ProductRepository.java
+++ src/main/java/se/iths/kattis/webshopgrupp5/repository/ProductRepository.java
@@ -1,10 +1,12 @@
 package se.iths.kattis.webshopgrupp5.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import se.iths.kattis.webshopgrupp5.model.Product;
 
 import java.util.List;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByProductId(Long productId);
 
