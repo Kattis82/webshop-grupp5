15f46588a548637fbfd04970ef62351d2b4c989c
J0ssan-iths <Josefine.Berglund@iths.se>
2026-04-09 15:23:07 +0200

    Lagt till findByProductname i repository, skapat en prpduct exception klass och ProductService

---
+++ src/main/java/se/iths/kattis/webshopgrupp5/service/ProductService.java
@@ -0,0 +1,58 @@
+package se.iths.kattis.webshopgrupp5.service;
+
+import org.springframework.stereotype.Service;
+import se.iths.kattis.webshopgrupp5.exception.ProductNotFoundException;
+import se.iths.kattis.webshopgrupp5.model.AppUser;
+import se.iths.kattis.webshopgrupp5.model.Product;
+import se.iths.kattis.webshopgrupp5.repository.ProductRepository;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private ProductRepository productRepository;
+    private Product product;
+    private AppUser appUser;
+
+    public ProductService() {
+    }
+
+    public ProductService(Product product, ProductRepository productRepository, AppUser appUser) {
+        this.product = product;
+        this.productRepository = productRepository;
+        this.appUser = appUser;
+    }
+
+    //visa alla produkter
+    public List<Product> showAllProducts() {
+        List<Product> products = productRepository.findAll();
+        return products;
+    }
+
+    //sortera per kategori
+    public List<Product> sortByCategory(String category) {
+        List<Product> categorys = productRepository.findByCategory(category);
+        return categorys;
+    }
+
+    //skapa ny product om admin
+    public Product createProduct(String name, Double price, String category, String pictureUrl) {
+        if (appUser.getRole().equals("ADMIN")) {
+            Product product = new Product(name, price, category, pictureUrl);
+            return product;
+        }
+        return null;
+    }
+
+    //Hitta produkt via namn
+    public List<Product> findByProductname(String name) {
+        try {
+            List<Product> product = productRepository.findByProductname(name);
+            return product;
+        } catch (ProductNotFoundException e) {
+            System.out.println("Product not found");
+            return null;
+        }
+    }
+}
