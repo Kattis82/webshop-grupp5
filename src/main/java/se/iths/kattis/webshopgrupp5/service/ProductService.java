a0c7b281d2735bf005103d66f0e05ef0c90aaeeb
J0ssan-iths <Josefine.Berglund@iths.se>
2026-04-10 10:16:39 +0200

    Ändringar i service efter feedback

--- src/main/java/se/iths/kattis/webshopgrupp5/service/ProductService.java
+++ src/main/java/se/iths/kattis/webshopgrupp5/service/ProductService.java
@@ -1,8 +1,6 @@
 package se.iths.kattis.webshopgrupp5.service;
 
 import org.springframework.stereotype.Service;
-import se.iths.kattis.webshopgrupp5.exception.ProductNotFoundException;
-import se.iths.kattis.webshopgrupp5.model.AppUser;
 import se.iths.kattis.webshopgrupp5.model.Product;
 import se.iths.kattis.webshopgrupp5.repository.ProductRepository;
 
@@ -12,48 +10,24 @@ import java.util.List;
 public class ProductService {
 
     private ProductRepository productRepository;
-    private Product product;
-    private AppUser appUser;
 
-    public ProductService() {
-    }
 
-    public ProductService(Product product, ProductRepository productRepository, AppUser appUser) {
-        this.product = product;
+    public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
-        this.appUser = appUser;
     }
 
     //visa alla produkter
-    public List<Product> showAllProducts() {
-        List<Product> products = productRepository.findAll();
-        return products;
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
     }
 
     //sortera per kategori
     public List<Product> sortByCategory(String category) {
-        List<Product> categorys = productRepository.findByCategory(category);
-        return categorys;
-    }
-
-    //skapa ny product om admin
-    public Product createProduct(String name, Double price, String category, String pictureUrl) {
-        if (appUser.getRole().equals("ADMIN")) {
-            Product product = new Product(name, price, category, pictureUrl);
-            productRepository.save(product);
-            return product;
-        }
-        return null;
+        return productRepository.findByCategory(category);
     }
 
     //Hitta produkt via namn
     public List<Product> findByProductname(String name) {
-        try {
-            List<Product> product = productRepository.findByName(name);
-            return product;
-        } catch (ProductNotFoundException e) {
-            System.out.println("Product not found");
-            return null;
-        }
+        return productRepository.findByName(name);
     }
 }
