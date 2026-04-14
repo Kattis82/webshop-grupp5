5851824d789c19d162228d4fa1a3069e61598a48
Johannavii <jh.viippa@gmail.com>
2026-04-14 10:17:04 +0200

    Skapat ett service-lager för AppUser: register, find, delete, exists.

---
+++ src/main/java/se/iths/kattis/webshopgrupp5/service/AppUserService.java
@@ -0,0 +1,42 @@
+package se.iths.kattis.webshopgrupp5.service;
+
+import org.springframework.stereotype.Service;
+import se.iths.kattis.webshopgrupp5.model.AppUser;
+import se.iths.kattis.webshopgrupp5.repository.AppUserRepository;
+
+import java.util.Optional;
+
+@Service
+public class AppUserService {
+
+    // App repository
+    private AppUserRepository repository;
+
+    // Konstruktor
+    public AppUserService(AppUserRepository repository) {
+        this.repository = repository;
+    }
+
+    // Metod - Registrar användare
+    public void register(String username, String password, boolean consent) {
+        AppUser user = new AppUser();
+        user.setUsername(username);
+        user.setPassword(password);
+        repository.save(user);
+    }
+
+    // Metod - Hämtar via användare
+    public Optional<AppUser> findByUsername(String username) {
+        return repository.findByUsername(username);
+    }
+
+    // Metod - Tar bort användare
+    public void deleteByUsername(String username) {
+        repository.deleteByUsername(username);
+    }
+
+    // Metod - Kollar om email existerar
+    public boolean existsByUsername(String username) {
+        return repository.existsByUsername(username);
+    }
+}
