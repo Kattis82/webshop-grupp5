d3d08fc1dee99da45f95a6295044a9fd03c98a59
Johannavii <jh.viippa@gmail.com>
2026-04-17 13:12:58 +0200

    Lagt till saknad @ModelAttribute i RegistrationController

--- src/main/java/se/iths/kattis/webshopgrupp5/controller/RegistrationController.java
+++ src/main/java/se/iths/kattis/webshopgrupp5/controller/RegistrationController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import se.iths.kattis.webshopgrupp5.model.RegistrationForm;
@@ -31,7 +32,7 @@ public class RegistrationController {
 
     // Metod - Hantera registrering
     @PostMapping
-    public String handleForm(@Valid RegistrationForm form,
+    public String handleForm(@Valid @ModelAttribute("form") RegistrationForm form,
                              BindingResult result,
                              Model model) {
 
