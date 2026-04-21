1a251e50ac38f5ce78a4d9bded1d8c277082c8da
Johannavii <jh.viippa@gmail.com>
2026-04-21 11:19:00 +0200

    Lagt till AppUserService - Mockito-tester (register, find, delete, exists)

--- src/test/java/se/iths/kattis/webshopgrupp5/WebshopGrupp5ApplicationTests.java
+++ src/test/java/se/iths/kattis/webshopgrupp5/WebshopGrupp5ApplicationTests.java
@@ -2,8 +2,10 @@ package se.iths.kattis.webshopgrupp5;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class WebshopGrupp5ApplicationTests {
 
     @Test
